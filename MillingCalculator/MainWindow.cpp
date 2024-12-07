9b09fcb367965d7901c5811059819df4a36adb79
ZhangPuyu-WorkStation <zhang.puyu@foxmail.com>
2024-12-07 15:48:22 +0800

    support Bull/Ball tool

--- MillingCalculator/MainWindow.cpp
+++ MillingCalculator/MainWindow.cpp
@@ -2,6 +2,8 @@
 
 #include "qpixmap.h"
 #include "qstring.h"
+#include "qmessagebox.h"
+#include "qdebug.h"
 
 #pragma execution_character_set("utf-8")
 
@@ -23,21 +25,38 @@ MainWindow::MainWindow(QWidget *parent)
         ui->vc->setValue(pie * D() * N() / 1000);
         ui->fz->setValue(Vf() / (Z() * N()));
         });
-
     connect(ui->btnFz, &QToolButton::clicked, [=]() {
         ui->rev->setValue(Vc() / (pie * D()) * 1000);
         ui->feed->setValue(Fz() * Z() * N());
         });
-
 	connect(ui->btnVc, &QToolButton::clicked, [=]() {
         ui->rev->setValue(Vc() / (pie * D()) * 1000);
         ui->feed->setValue(Fz() * Z() * N());
 		});
 
+	connect(ui->radius, &QDoubleSpinBox::editingFinished, [=]() {
+		if (!checkValue())
+		{
+            ui->radius->setValue(0);
+			QMessageBox::critical(this, tr("错误"), tr("r角必须小于刀具半径"));
+		}
+        else
+            ui->statusBar->showMessage(tr("环形刀/球头刀等效直径：")+ QString::number(D(), 'f', 2));
+		});
+    connect(ui->height, &QDoubleSpinBox::editingFinished, [=]() {
+            ui->statusBar->showMessage(tr("环形刀/球头刀等效直径：") + QString::number(D(), 'f', 2));
+        });
+
     emit ui->btnFz->clicked();
 	emit ui->btnVc->clicked();
 
-    QLabel* copyright = new QLabel("(C) 2024 张璞玉  ");
+    QLabel* copyright = new QLabel("Copyright(C) 2024 张璞玉  ");
     ui->statusBar->addPermanentWidget(copyright);
-    ui->statusBar->showMessage(tr("单位和UG.NX一致"));
+    ui->statusBar->showMessage(tr("单位和 UG.NX 一致"));
+}
+
+bool MainWindow::checkValue()
+{
+    double d = ui->diameter->value();
+    return d + 0.0001 > r() * 2;
 }
--- MillingCalculator/MainWindow.h
+++ MillingCalculator/MainWindow.h
@@ -7,6 +7,7 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindowClass; };
 QT_END_NAMESPACE
 
+/// @brief 主窗口
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -19,11 +20,30 @@ private:
 
     const double pie = 3.14159;
 
-    double D()  { return ui->diameter->value(); }
+    /// @brief 环形刀(牛鼻刀)在距离底部H处的等效直径
+    double D()  
+    { 
+        double R = ui->diameter->value() / 2; 
+        if (h() >= r())
+            return R * 2;
+        else
+            return (R - r() + sqrt(2 * r() * h() - h() * h())) * 2;
+    }
+    /// @brief 刀齿数
     int    Z()  { return ui->toothNum->value(); }
-
+	/// @brief 环形刀(牛鼻刀)底部R角半径
+	double r()  { return ui->radius->value(); }
+	/// @brief 刀具横截面距离底部的高度
+	double h()  { return ui->height->value(); }
+    /// @brief 主轴转速
     double N()  { return ui->rev->value();  }
+	/// @brief 进给速度
     double Vf() { return ui->feed->value(); }
+	/// @brief 每齿进给量
     double Fz() { return ui->fz->value();   }
+	/// @brief 表面速度
     double Vc() { return ui->vc->value();   }
+
+    /// @brief 检查数据
+    bool checkValue();
 };
